5ab40f1a7739008d5e97dd7ad916bf94c8583ad6
kozuberda <tanya1frolova@gmail.com>
2016-02-14 22:38:44 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-$(document).foundation();
+;(function($){
+	$(function(){
+		$('.ba-slider').slick ({
+			dots: false,
+   			infinite:false,
+			arrow:true
+
+		});
+
+});
+
+ 	$(function(){
+		$('.ba-browser-slaider').slick ({
+			dots: true,
+   			infinite:false,
+ 			arrow:false,
+ 			prevArrow: false,
+			nextArrow:false
+		});
+
+ });
+
+
+})(jQuery);
